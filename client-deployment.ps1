a09b07ac4bfd5922451b1d7df0200fe80115d812
NB-Cooperation <142782596+NB-Cooperation@users.noreply.github.com>
2025-04-01 12:28:08 +0000

    Update client-deployment.ps1

--- client-deployment.ps1
+++ client-deployment.ps1
@@ -1,22 +1,9 @@
 $ErrorActionPreference= 'silentlycontinue'
 
-# Assign the value random password to the password variable
-$rustdesk_pw=(-join ((65..90) + (97..122) | Get-Random -Count 12 | % {[char]$_}))
-
 # Get your config string from your Web portal and Fill Below
 $rustdesk_cfg="Qfi0zawREShBnaPFUTQxUT1dFZP5GVvpVc0RnaGJWckVnR24WbUxmNGFnYOZ2ViojI5V2aiwiIiojIpBXYiwiIlRmLu9Wa0FmclB3bvNWLi5mLrNXZkR3c1JnI6ISehxWZyJCLiUGZu42bpRXYyVGcv92YtImbus2clRGdzVnciojI0N3boJye"
 
 ################################### Please Do Not Edit Below This Line #########################################
-
-# Run as administrator and stays in the current directory
-if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000)
-    {
-        Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
-        Exit;
-    }
-}
 
 # This function will return the latest version and download link as an object
 function getLatest()
@@ -61,56 +48,9 @@ function getLatest()
 
 $RustDeskOnGitHub = getLatest
 
-
-$rdver = ((Get-ItemProperty  "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\RustDesk\").Version)
-
-if ($rdver -eq $RustDeskOnGitHub.Version)
-{
-    Write-Output "RustDesk $rdver is the newest version."
-    Exit
-}
-
-if (!(Test-Path C:\Temp))
-{
-    New-Item -ItemType Directory -Force -Path C:\Temp | Out-Null
-}
-
-cd C:\Temp
+cd $env:USERPROFILE\Downloads
 
 Invoke-WebRequest $RustDeskOnGitHub.Downloadlink -Outfile "rustdesk.exe"
-Start-Process .\rustdesk.exe --silent-install
-Start-Sleep -seconds 20
-
-$ServiceName = 'Rustdesk'
-$arrService = Get-Service -Name $ServiceName -ErrorAction SilentlyContinue
-
-if ($arrService -eq $null)
-{
-    Write-Output "Installing service"
-    cd $env:ProgramFiles\RustDesk
-    Start-Process .\rustdesk.exe --install-service
-    Start-Sleep -seconds 20
-    $arrService = Get-Service -Name $ServiceName
-}
-
-while ($arrService.Status -ne 'Running')
-{
-    Start-Service $ServiceName
-    Start-Sleep -seconds 5
-    $arrService.Refresh()
-}
-
-cd $env:ProgramFiles\RustDesk\
-.\rustdesk.exe --get-id | Write-Output -OutVariable rustdesk_id
-
-.\rustdesk.exe --config $rustdesk_cfg
-
-.\rustdesk.exe --password $rustdesk_pw
-
-Write-Output "..............................................."
-# Show the value of the ID Variable
-Write-Output "RustDesk ID: $rustdesk_id"
-
-# Show the value of the Password Variable
-Write-Output "Password: $rustdesk_pw"
-Write-Output "..............................................."
+.\rustdesk.exe 
+Start-Sleep -seconds 2
+.\rustdesk.exe -config $rustdesk_cfg
